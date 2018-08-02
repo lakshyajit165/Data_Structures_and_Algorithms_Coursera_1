7a8a7ff4f35ee5070a5766a399488b2ee8f5834c
lakshyajit165 <lakshyajit165@gmail.com>
2018-08-03 00:34:39 +0530

    Week 3 assignment

--- programs_for_practice/last_digit_of_sum_of_fibo.cpp
+++ programs_for_practice/last_digit_of_sum_of_fibo.cpp
@@ -1,21 +1,22 @@
 #include<iostream>
 using namespace std;
-long long Fibo(long n){
-	long long A[n];
-	long long sum = 0;
-	A[0] = 0;
-	A[1] = 1;
-	for(int i = 2; i<n; i++){
-		A[i] = A[i-1]+A[i-2];
-	}
-	for(int i = 0; i<n; i++)
-		sum+=A[i];
-	return(sum%10);	
+long long calc_last_digit(long long n){
+	if(n <= 1)
+		return(n);
+	else{
+		long long A[n+1];
+		A[0] = 0;
+		A[1] = 1;
+		for(long long i = 2; i < (n+1); i++)
+			A[i] = A[i-1] + A[i-2];
+		return(A[n]-1);		
+	}	
 }
-int main()
-{
-	int n;
+int main(){
+	
+	long long n,res;
 	cin>>n;
-	cout<<Fibo(n);
+	res = calc_last_digit(n+2)-1;
+	cout<<res;
 	return 0;
 }
